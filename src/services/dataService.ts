047714d7bc2de17f34c3000a72790825ae8a3229
Raúl Alves <raulalves32@gmail.com>
2024-09-12 15:47:21 +0100

    src: remove unused components and services

--- src/services/dataService.ts
+++
@@ -1,7 +0,0 @@
-import Papa from 'papaparse';
-
-export const loadBirdFluData = async () => {
-    const response = await fetch('/path-to-dataset.csv');
-    const csvData = await response.text();
-    return Papa.parse(csvData, { header: true }).data;
-};
